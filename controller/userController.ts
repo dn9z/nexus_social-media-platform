9451a97908885b5974255c43dd686442b9b31b9b
dn9z <28568855+dn9z@users.noreply.github.com>
2022-06-15 12:14:06 +0200

    server boilerplate added

---
+++ controller/userController.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express'
+
+export async function test(req:Request,res:Response){
+  try {
+    return res.status(200).json({ message: "User Created" });
+  } catch (error) {
+    return res.status(400).json({ message: "Something went wrong creating the user", error });
+  }
+}
+
+export default { test };
---
+++ server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose, {ConnectOptions} from "mongoose";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import userRoutes from './routes/userRoutes'
+
+dotenv.config()
+const app = express()
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions // needs to be added for typescript
+  )
+  .then(() => {
+    console.log("we are connected to the database.");
+  })
+  .catch((error) => {
+    console.log("an error occurred while connecting to the db", error);
+  });
+
+
+
+  app.use("/api/users", userRoutes);
+
+app.listen(app.get('port'), () => {
+  console.log('Server is listening on port', app.get('port'))
+})
