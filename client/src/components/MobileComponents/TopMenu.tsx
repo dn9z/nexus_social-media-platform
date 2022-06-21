513cada074418965dc2860be4c9234fbf567c162
MartinJ <MrTinsk@protonmail.com>
2022-06-21 09:48:53 +0200

    Mobile View Main Page With Nav Bar

--- client/src/components/MobileComponents/TopMenu.tsx
+++ client/src/components/MobileComponents/TopMenu.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import styled from 'styled-components'
+import * as React from "react";
+import styled from "styled-components";
 import { Context } from "../../context/Context";
-import Hamburger from '../../icons/Hamburger';
-import User from '../../icons/User';
-import * as themeConf from "../../styles/theme"
-import HamburgerMenu from './HamburgerMenu'
-import {useState} from 'react'
+import Hamburger from "../../icons/Hamburger";
+import User from "../../icons/User";
+import * as themeConf from "../../styles/theme";
+import HamburgerMenu from "./HamburgerMenu";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -13,53 +13,43 @@ const Container = styled.div`
   justify-content: space-between;
   position: sticky;
   top: 0px;
-  background-color: rgb(51,51,51);
+  background-color: rgb(51, 51, 51);
   z-index: 10;
-`
-const Label = styled.p`
+`;
+const Label = styled.span`
   color: springgreen;
   font-family: Quicksand;
   letter-spacing: 1rem;
-`
+`;
 
 const Item = styled.button`
   all: unset;
-  &:active;
+  &: active;
   &:hover {
     background-color: springgreen;
   }
 `;
 
-
-
 const TopMenu: React.FC = () => {
-    const context = React.useContext(Context);
-    const [hidden, setHidden] = useState(true);
+  const context = React.useContext(Context);
+  const [hidden, setHidden] = useState(true);
 
   return (
-    
     <Container>
-      
-      
-      <Item onClick={() => setHidden(s => !s)} >
-      <Hamburger color={context.color}/>
-      {!hidden ? <HamburgerMenu/> : null}
+      <Item onClick={() => setHidden((s) => !s)}>
+        <Hamburger color={context.color} />
+        {!hidden ? <HamburgerMenu /> : null}
       </Item>
-
-      
 
       <Label>
         <h2>NEXUS</h2>
       </Label>
 
       <Item onClick={(event) => context.handleClick(event)}>
-        <User color={context.color}/>
+        <User color={context.color} />
       </Item>
+    </Container>
+  );
+};
 
-      </Container>
-
-     
-  )
-}
-
-export default TopMenu
+export default TopMenu;
