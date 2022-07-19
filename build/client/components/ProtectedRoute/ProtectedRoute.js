8ab902bab2cffdbf92e85de72577303332b2557e
unknown <28568855+dn9z@users.noreply.github.com>
2022-07-19 23:24:26 +0200

    deployment test

--- build/client/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Navigate } from "react-router-dom";
-var ProtectedRoute = function (_a) {
-    var children = _a.children, auth = _a.auth;
-    //auth = true --> render children in the other case navigate to the login page.
-    return auth ? children : _jsx(Navigate, { to: "/login" });
-};
-export default ProtectedRoute;
