0a8c0c464fcff210f60e486e9b8864ffd068df71
unknown <28568855+dn9z@users.noreply.github.com>
2022-07-20 01:01:21 +0200

    deployment test

--- build/client/components/MobileComponents/Home/HomeIndex.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import MobileFeed from '../Feed/MobileFeed';
-import MobileGroups from "../MobileGroups";
-import TopMenu from '../TopMenu';
-var MobileMain = function () {
-    return (_jsxs(_Fragment, { children: [_jsx(TopMenu, {}), _jsx(MobileGroups, {}), _jsx(MobileFeed, {})] }));
-};
-export default MobileMain;
